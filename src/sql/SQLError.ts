d2d82c351ea6acb311d7581010585a7260f41bec
Dimi Kot <dimi.kot.code@gmail.com>
2022-02-18 17:15:19 -0800

    Remove esnext native private props from ent-framework

--- src/sql/SQLError.ts
+++ src/sql/SQLError.ts
@@ -1,9 +1,11 @@
 import { copyStack } from "../helpers";
 
 export class SQLError extends Error {
-  #sql: string;
-
-  constructor(public readonly origError: any, destName: string, sql: string) {
+  constructor(
+    public readonly origError: any,
+    destName: string,
+    public readonly sql: string
+  ) {
     super(origError.message);
 
     Object.defineProperty(this, "name", {
@@ -11,21 +13,15 @@ export class SQLError extends Error {
       writable: true,
       enumerable: false,
     });
-    this.#sql = sql;
+    Object.defineProperty(this, "sql", {
+      value: sql,
+      writable: false,
+      enumerable: false,
+    });
 
     copyStack(this, origError);
     this.stack += "\n" + destName + ": " + sql.replace(/\s*\n\s*/g, " ");
     delete origError.stack;
-  }
-
-  /**
-   * We could've just make this.sql a readonly property (and not #sql) instead,
-   * but then it'd be exposed when printing the error via inspect() or
-   * console.log(), which we don't want to. So instead, we define it as a getter
-   * function (which is "invisible" when using e.g. console.log()).
-   */
-  sql() {
-    return this.#sql;
   }
 
   isFKError(fkName?: string) {
