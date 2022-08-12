754d67c987594cf418384b45d570dba8aca163e7
Dimi Kot <dimi.kot.code@gmail.com>
2022-08-12 02:03:06 -0700

    Allow fallback to random shard when inserting a sharded Ent (like EntCred) whose shardAffinity field (like tenant_id) points to global shard

--- src/ent/ShardLocator.ts
+++ src/ent/ShardLocator.ts
@@ -24,14 +24,22 @@ export class ShardLocator<TClient extends Client, TField extends string> {
 
   /**
    * Called in a context when we must know exactly 1 shard to work with (e.g.
-   * INSERT, UPSERT etc.).
+   * INSERT, UPSERT etc.). If fallbackToRandomShard is true, then returns a
+   * random shard in case when it can't infer the shard number from the input
+   * (used in e.g. INSERT operations); otherwise throws
+   * EntCannotDetectShardError (happens in e.g. upsert, loadBy etc.).
    */
   singleShardFromInput(
     input: Record<string, any>,
     op: string,
-    allowRandomShard: boolean
+    fallbackToRandomShard: boolean
   ): Shard<TClient> {
-    const shard = this.shardFromAffinity(input, allowRandomShard);
+    let shard = this.shardFromAffinity(input);
+
+    if (!shard && fallbackToRandomShard) {
+      shard = this.cluster.randomShard();
+    }
+
     if (!shard) {
       throw new EntCannotDetectShardError(
         this.schemaName,
@@ -56,7 +64,7 @@ export class ShardLocator<TClient extends Client, TField extends string> {
     input: Record<string, any>,
     op: string
   ): Promise<Array<Shard<TClient>>> {
-    const singleShard = this.shardFromAffinity(input, false);
+    const singleShard = this.shardFromAffinity(input);
     if (singleShard) {
       return [singleShard];
     }
@@ -71,7 +79,10 @@ export class ShardLocator<TClient extends Client, TField extends string> {
         await mapJoin(id1 instanceof Array ? id1 : [id1], async (id1) => {
           const id2s = await inverse.id2s(vc, id1);
           for (const id2 of id2s) {
-            shards.add(this.singleShardFromID(field, id2));
+            const shard = this.singleShardFromID(field, id2);
+            if (shard) {
+              shards.add(shard);
+            }
           }
         });
       }
@@ -96,14 +107,20 @@ export class ShardLocator<TClient extends Client, TField extends string> {
   }
 
   /**
-   * A wrapper for cluster.shard() which injects Ent name to the exception. This
-   * is just a convenience for debugging.
+   * A wrapper for cluster.shard() which injects Ent name to the exception (in
+   * case of e.g. "Cannot locate shard" exception). This is just a convenience
+   * for debugging.
+   *
+   * If this method returns null, that means the caller show give up trying to
+   * load the Ent with this ID, because it won't find it anyways (e.g. when we
+   * try to load a sharded Ent using an ID from the global shard). This is
+   * identical to the case of an Ent not existing in the database.
    */
   singleShardFromID(field: string, id: string | null | undefined) {
-    // ATTENTION: GLOBAL_SHARD has precedence over a shard number from ID (or
-    // any other fields, since global tables may refer to global tables only).
-    // This allows to move some previously sharded objects to the global shard
-    // while doing some refactoring.
+    // GLOBAL_SHARD has precedence over a shard number from ID (or any other
+    // fields, since global tables may refer to global tables only). This allows
+    // to move some previously sharded objects to the global shard while doing
+    // some refactoring.
     if (this.shardAffinity === GLOBAL_SHARD) {
       return this.cluster.globalShard();
     }
@@ -111,13 +128,14 @@ export class ShardLocator<TClient extends Client, TField extends string> {
     try {
       const shard = this.cluster.shard(nullthrows(id));
       if (shard.no === this.cluster.globalShard().no) {
-        throw Error(
-          "ID is from the global shard, although the Ent has a non-global shard affinity"
-        );
+        // We're trying to load a sharded Ent using an ID from the global shard.
+        // We know for sure that there will be no such Ent there then.
+        return null;
+      } else {
+        return shard;
       }
-
-      return shard;
     } catch (origError) {
+      // E.g. "shard does not exist" error; we don't want to mute it.
       if (origError instanceof Error) {
         const error = new EntNotFoundError(
           this.schemaName + (field !== ID ? "." + field : ""),
@@ -125,9 +143,9 @@ export class ShardLocator<TClient extends Client, TField extends string> {
           origError.message
         );
         throw copyStack(error, origError);
+      } else {
+        throw origError;
       }
-
-      throw origError;
     }
   }
 
@@ -140,10 +158,12 @@ export class ShardLocator<TClient extends Client, TField extends string> {
     );
   }
 
-  private shardFromAffinity(
-    input: Record<string, any>,
-    allowRandomShard: boolean
-  ): Shard<TClient> | null {
+  /**
+   * Infers shard number from shardAffinity info and the input record. Returns
+   * null if it can't do this; the caller should likely throw in this case
+   * (although not always).
+   */
+  private shardFromAffinity(input: Record<string, any>): Shard<TClient> | null {
     // For a low number of a very global objects only. ATTENTION: GLOBAL_SHARD
     // has precedence over a shard number from ID! This allows to move some
     // previously sharded objects to the global shard while doing some
@@ -169,13 +189,9 @@ export class ShardLocator<TClient extends Client, TField extends string> {
           return this.singleShardFromID(fromField, input[fromField].toString());
         }
       }
-
-      // RANDOM_SHARD works for INSERT only; for other operations, we must know
-      // the shard in advance (either from ID or by deducing by affinity).
-      return allowRandomShard ? this.cluster.randomShard() : null;
     }
 
-    // Something weird, should never happen.
+    // Couldn't detect shard number from any of the sources.
     return null;
   }
 }
