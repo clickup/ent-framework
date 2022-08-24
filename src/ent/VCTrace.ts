7ad718b5e45a19cb77934c2deff4fdd36aee1629
Dimi Kot <dimi.kot.code@gmail.com>
2022-08-23 21:07:02 -0400

    feat(slapdash) Add Datadog logging

--- src/ent/VCTrace.ts
+++ src/ent/VCTrace.ts
@@ -1,5 +1,4 @@
-import uniqid from "uniqid";
-
+import { randomBytes } from "crypto";
 /**
  * A "trace" objects which allows to group database related stuff while logging
  * it. Traces are inherited during VC derivation.
@@ -7,7 +6,22 @@ import uniqid from "uniqid";
 export class VCTrace {
   readonly trace: string;
 
-  constructor(readonly prefix = "") {
-    this.trace = (prefix ? prefix + "-" : "") + uniqid();
+  constructor(
+    /** stringified uint64 (0 - 18446744073709551615) */
+    readonly rawTrace?: string,
+    readonly prefix = ""
+  ) {
+    // rawTrace will be missing if DD is logging disabled or there is no "root" span.
+    // We don't need to fake it if it is not provided.
+    this.trace =
+      (prefix ? prefix + "-" : "") + this.rawTrace || createRandomRawTrace();
   }
+}
+
+function createRandomRawTrace() {
+  const buf = randomBytes(8);
+  return (
+    BigInt(buf.readUInt32BE(0)) +
+    (BigInt(buf.readUInt32BE(4)) << BigInt(32))
+  ).toString();
 }
