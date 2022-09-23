495dd4ecd869b30774732d77f7c463a8af781b54
Dimi Kot <dimi.kot.code@gmail.com>
2022-09-23 01:27:46 -0700

    Add code comments o Loader.ts

--- src/abstract/Loader.ts
+++ src/abstract/Loader.ts
@@ -3,20 +3,29 @@ import pDefer from "p-defer";
 
 export interface Handler<TLoadArgs extends any[], TReturn> {
   onCollect: (...args: TLoadArgs) => void;
-  onReturn: (...args: TLoadArgs) => TReturn;
   onFlush: () => Promise<void>;
+  onReturn: (...args: TLoadArgs) => TReturn;
 }
 
 /**
  * Loader allows to batch single-item requests into batches. It uses a different
  * architecture than Facebook's DataLoader:
- * - it's more developers-friendly: multi-parameter loadings, automatic
- *   deduplication of requests, no requirement to serialize/deserialize requests
- *   into string keys;
+ *
+ * - it's more developers-friendly: multi-parameter loadings, you may implement
+ *   automatic deduplication of requests at onCollect stage, no requirement to
+ *   serialize/deserialize requests into string keys;
  * - strong-typed load() and handler arguments.
  *
- * To create your own specific loader, define a handler class with
- * onCollect/onReturn/onFlush methods.
+ * To create your own specific loader:
+ * 1. Define a handler class with onCollect/onReturn/onFlush methods.
+ * 2. In onCollect, accumulate the incoming requests in the handler object's
+ *    private property.
+ * 3. In onFlush, process what you accumulated so far and save to another
+ *    handler object's private property (and by adding, say, delay(50) in the
+ *    beginning of onFlush, you may group the requests coming within the 1st 50
+ *    ms).
+ * 3. In onReturn, extract the result corresponding to the request and return
+ *    it, so the caller will receive it seamlessly as a load() return value.
  *
  * In the future, Batcher may be refactored to use Loader as the underlying
  * engine, but for now they're separate (Batcher is much more domain logic
