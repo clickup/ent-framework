dcba73fc94e37eb91ae0065a36895e4aec1b682e
Dimi Kot <dimi.kot.code@gmail.com>
2022-11-16 15:50:08 -0800

    Improve inverses batching

--- src/ent/Inverse.ts
+++ src/ent/Inverse.ts
@@ -1,21 +1,23 @@
+import flatten from "lodash/flatten";
 import type { Client } from "../abstract/Client";
 import type { Cluster } from "../abstract/Cluster";
 import type { Query } from "../abstract/Query";
 import type { Schema } from "../abstract/Schema";
+import type { Shard } from "../abstract/Shard";
+import { DefaultMap } from "../helpers/DefaultMap";
 import Memoize from "../helpers/Memoize";
-import { join } from "../helpers/misc";
-import type { IDFieldsRequired, Table } from "../types";
+import { join, mapJoin } from "../helpers/misc";
+import type { IDFieldsRequired, Row, Table } from "../types";
 import { ID } from "../types";
 import type { ShardAffinity } from "./Configuration";
 import { GLOBAL_SHARD } from "./Configuration";
 import type { VC } from "./VC";
 
-// For perf reasons, we return no more than that number of id2s per each id1.
-const MAX_ID2_PER_ID1 = 1000;
-
-// No DB unique indexes can include a nullable field and be really unique, so we
-// simulate id1=NULL via just storing "0" in the inverse, and Inverse abstracts
-// this fact from the caller.
+/**
+ * No DB unique indexes can include a nullable field and be really unique, so we
+ * simulate id1=NULL via just storing "0" in the inverse, and Inverse abstracts
+ * this fact from the caller.
+ */
 const ZERO_NULL = "0";
 
 /**
@@ -26,12 +28,13 @@ const ZERO_NULL = "0";
  * field "org_id" in EntUser refers an EntOrg row).
  */
 export class Inverse<TClient extends Client, TTable extends Table> {
-  public readonly cluster;
-  public readonly shardAffinity;
-  public readonly inverseSchema;
   public readonly id2Field;
-  public readonly name;
-  public readonly type;
+  private cluster;
+  private shardAffinity;
+  private name;
+  private type;
+  private inverseSchema;
+  private InverseLoader;
 
   constructor({
     cluster,
@@ -54,6 +57,7 @@ export class Inverse<TClient extends Client, TTable extends Table> {
     this.id2Field = id2Field;
     this.name = name;
     this.type = type;
+    this.InverseLoader = this.buildInverseLoaderClass();
   }
 
   /**
@@ -66,7 +70,7 @@ export class Inverse<TClient extends Client, TTable extends Table> {
 
     await this.run(
       vc,
-      id1,
+      this.shard(id1),
       this.inverseSchema.insert({ id1: id1 ?? ZERO_NULL, type: this.type, id2 })
     );
   }
@@ -100,11 +104,11 @@ export class Inverse<TClient extends Client, TTable extends Table> {
 
     const row = await this.run(
       vc,
-      id1,
+      this.shard(id1),
       this.inverseSchema.loadBy({ id1: id1 ?? "0", type: this.type, id2 })
     );
     if (row) {
-      await this.run(vc, id1, this.inverseSchema.delete(row.id));
+      await this.run(vc, this.shard(id1), this.inverseSchema.delete(row.id));
     }
   }
 
@@ -113,14 +117,7 @@ export class Inverse<TClient extends Client, TTable extends Table> {
    * rows is limited to not overload the database.
    */
   async id2s(vc: VC, id1: string | null) {
-    const rows = await this.run(
-      vc,
-      id1,
-      this.inverseSchema.select({
-        where: { id1: id1 ?? ZERO_NULL, type: this.type },
-        limit: MAX_ID2_PER_ID1,
-      })
-    );
+    const rows = await vc.loader(this.InverseLoader).load(id1);
     return rows.map((row) => row.id2).sort();
   }
 
@@ -148,40 +145,107 @@ export class Inverse<TClient extends Client, TTable extends Table> {
    */
   private async run<TOutput>(
     vc: VC,
-    id1: string | null,
+    shard: Shard<TClient>,
     query: Query<TOutput>
   ) {
-    // id1=NULL inverse is always put to the global shard.
-    const shard = id1 ? this.cluster.shard(id1) : this.cluster.globalShard();
     return shard.run(
       query,
       vc.toAnnotation(),
-      vc.timeline(shard, this.name + ":" + this.type),
+      vc.timeline(shard, `${this.name}:${this.type}`),
       vc.freshness
     );
   }
 
   /**
+   * Returns a target shard for an id.
+   */
+  private shard(id: string | null) {
+    // id1=NULL inverse is always put to the global shard.
+    return id ? this.cluster.shard(id) : this.cluster.globalShard();
+  }
+
+  /**
    * Creates an inverse schema which derives its id field's autoInsert from the
    * passed id2 schema. The returned schema is heavily cached, so batching for
-   * it works efficiently even for different id2 schemas.
+   * it works efficiently even for different id2 schemas and different inverse
+   * types (actually, it would work the same way even without @Memoize since
+   * Runner batches by schema hash, not by schema object instance, but anyways).
    */
   @Memoize(
-    (schema: Schema<Table>, name: string) => schema.table[ID].autoInsert + name
+    (id2Schema: Schema<any>, name: string) =>
+      id2Schema.table[ID].autoInsert + name
   )
-  private static buildInverseSchema<TTable extends Table>(
-    schema: Schema<TTable>,
-    name: string
-  ) {
-    return new schema.constructor(
+  private static buildInverseSchema(id2Schema: Schema<any>, name: string) {
+    return new id2Schema.constructor(
       name,
       {
-        id: { type: ID, autoInsert: schema.table[ID].autoInsert! },
+        id: { type: ID, autoInsert: id2Schema.table[ID].autoInsert },
         type: { type: String },
         id1: { type: ID },
         id2: { type: ID },
       },
       ["id1", "type", "id2"]
     );
+  }
+
+  /**
+   * Allows Inverse.id2s() to build more efficient SQL query to load the
+   * requested inverses. Without InversesLoader, having 50 id1s even for the
+   * same type, Ent framework would issue a SELECT query with 50 "UNION ALL"
+   * clauses, and with this loader, there will be only a plain regular SELECT
+   * expression in this example.
+   *
+   * Why do we build this Loader class dynamically? Because the Loader is per
+   * inverse type: i.e. we make the type constant and then vary id1. We can't
+   * vary both of them, because e.g. in PG, there is no index-matching way of
+   * running the queries like:
+   *
+   * ```
+   * WHERE (id1, type) IN ((1, 'typeA'), ('2', 'typeA'), (3, 'typeB'))
+   * ```
+   *
+   * Despite the above syntax is formally supported in SQL and in PG, for some
+   * weird reason, PG doesn't utilize an index on (id1, type) efficiently on
+   * such queries; that's also the reason why SQLRunnerLoadBy only varies the
+   * last field in the unique key and not all of them.
+   */
+  private buildInverseLoaderClass() {
+    const self = this;
+
+    return class InverseLoader {
+      private id1sByShard = new DefaultMap<Shard<TClient>, Set<string>>();
+      private results = new DefaultMap<
+        string,
+        Array<Row<ReturnType<typeof Inverse.buildInverseSchema>["table"]>>
+      >();
+
+      constructor(private vc: VC) {}
+
+      onCollect(id1: string | null) {
+        this.id1sByShard.getOrAdd(self.shard(id1), Set).add(id1 ?? ZERO_NULL);
+      }
+
+      onReturn(id1: string | null) {
+        return this.results.get(id1 ?? ZERO_NULL) ?? [];
+      }
+
+      async onFlush() {
+        const rows = flatten(
+          await mapJoin([...this.id1sByShard], async ([shard, id1s]) =>
+            self.run(
+              this.vc,
+              shard,
+              self.inverseSchema.select({
+                where: { type: self.type, id1: [...id1s] },
+                limit: Number.MAX_SAFE_INTEGER,
+              })
+            )
+          )
+        );
+        for (const row of rows) {
+          this.results.getOrAdd(row.id1, Array).push(row);
+        }
+      }
+    };
   }
 }
