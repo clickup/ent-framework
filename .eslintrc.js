1be270576565fdd0f77f629e35a96ebbadb99212
Dimi Kot <dimi.kot.code@gmail.com>
2022-12-28 14:11:32 -0500

    Use non-client eslint config for non-client packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,4 @@
 "use strict";
-const config = require("../../.eslintrc.client.base.js")(__dirname);
+const config = require("../../.eslintrc.base.js")(__dirname);
 config.rules["import/no-extraneous-dependencies"] = "error";
 module.exports = config;
