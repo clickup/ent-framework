153c40f50c1b14c722839f8f774893359f513379
Dimi Kot <dimi.kot.code@gmail.com>
2023-01-07 22:21:35 -0800

    Return stricter lodash lint rules back

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,5 @@
 "use strict";
 const config = require("../../.eslintrc.base.js")(__dirname);
 config.rules["import/no-extraneous-dependencies"] = "error";
+config.rules["lodash/import-scope"] = ["error", "method"];
 module.exports = config;
