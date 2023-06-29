9467b0bd9027eb7968ca4e267e473daeaa025816
Dimi Kot <dimi.kot.code@gmail.com>
2023-06-28 21:39:22 -0700

    Add clear notion to dbValueToJs/stringify/parse in Ent Framework custom types

--- src/types.ts
+++ src/types.ts
@@ -31,7 +31,34 @@ export type SpecType =
   | typeof ID
   | typeof Number
   | typeof String
-  | { parse: (dbValue: any) => any; stringify: (jsValue: any) => string };
+  | {
+      /** Converts a value of some field returned by the low-level DB engine to
+       * its client representation, which can be reacher (e.g. support
+       * encryption/decryption). Notice that some DB engines already do some
+       * conversions internally: e.g. for node-postgres and an array field,
+       * dbValue returned by the engine is already an array of things, so
+       * dbValueToJs for it will likely do nothing. */
+      dbValueToJs: (dbValue: any) => any;
+      /** Converts a client value to the internal stringified representation of
+       * the low-level DB engine, which is suitable for injecting it into a
+       * plaintext query (with e.g. ?-placeholders).
+       * - Notice that this is intentionally NOT the opposite to dbValueToJs,
+       *   because it always needs to convert the value to a string, not to the
+       *   DB engine's row field type.
+       * - Example: node-postgres natively understands json/jsonb PG types and
+       *   can unescape them (called "PG type parsers" and mainly lives in
+       *   pg-types module; notice that there are no "PG type stringifiers
+       *   though"). The problem is that the low-level library's facilities for
+       *   escaping data is poor or doesn't exist (we do escaping by ourselves
+       *   for various reasons, like batching queries and better logging). So we
+       *   trust the library on the dbValueToJs path, but must manually
+       *   serialize on stringify path. */
+      stringify: (jsValue: any) => string;
+      /** The opposite to stringify function. Generally, it is not used on the
+       * read path (because the low level engine returns the rows suitable for
+       * dbValueToJs), but it's still here for completeness of the interface. */
+      parse: (str: string) => any;
+    };
 
 /**
  * { type: ..., ... } - one attribute spec.
@@ -106,8 +133,15 @@ export type ValueRequired<TSpec extends Spec> =
     ? string
     : TSpec["type"] extends typeof Date
     ? Date
-    : TSpec["type"] extends { parse: (dbValue: any) => infer TJSValue }
-    ? TJSValue
+    : TSpec["type"] extends {
+        dbValueToJs: (dbValue: any) => infer TJSValue;
+      }
+    ? TSpec["type"] extends {
+        stringify: (jsValue: TJSValue) => string;
+        parse: (str: string) => TJSValue;
+      }
+      ? TJSValue
+      : never
     : never;
 
 /**
